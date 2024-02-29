fffce3b4bd6f468930787697a2b20a8df8098b91
JaniBT <janoscsordas072@gmail.com>
2024-02-29 14:24:55 +0100

    updates

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,15 +1,34 @@
-document.addEventListener('focus', e => {
+document.addEventListener('focus', () => {
     document.title = "CyberNexus 🥵"
 })
 
-document.addEventListener('blur', e => {
+document.addEventListener('blur', () => {
     document.title = "CyberNexus szomorú 😭"
 })
 
 function openNav() {
-    document.getElementById("mySidebar").style.transform = "translateX(0)";
+    document.getElementById("mySidebar").style.transform = "translateX(0)"
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.transform = "translateX(100%)";
+    document.getElementById("mySidebar").style.transform = "translateX(100%)"
 }
+
+// Get the height of the viewport
+const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+// Function to scroll the page to a specific position
+function scrollToPosition(position) {
+  window.scrollTo({
+    top: position,
+    behavior: 'smooth' // Use smooth scrolling animation
+  });
+}
+
+// Scroll the page by one viewport height
+function scrollOneViewport() {
+  scrollToPosition(viewportHeight);
+}
+
+// Call the scrollOneViewport function when a button is clicked, for example
+document.querySelector('.scroll-button').addEventListener('click', scrollOneViewport);
