e0737c1e6a03d53bf5ccaa20db1f80e86c50f81b
JaniBT <janoscsordas072@gmail.com>
2024-02-29 18:58:02 +0100

    some updates

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -13,22 +13,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.transform = "translateX(100%)"
 }
-
-// Get the height of the viewport
-const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-
-// Function to scroll the page to a specific position
-function scrollToPosition(position) {
-  window.scrollTo({
-    top: position,
-    behavior: 'smooth' // Use smooth scrolling animation
-  });
-}
-
-// Scroll the page by one viewport height
-function scrollOneViewport() {
-  scrollToPosition(viewportHeight);
-}
-
-// Call the scrollOneViewport function when a button is clicked, for example
-document.querySelector('.scroll-button').addEventListener('click', scrollOneViewport);
