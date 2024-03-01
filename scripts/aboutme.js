34e6f1f0fc0b1e687fe8a063d3382e4daa88a6e6
JaniBT <janoscsordas072@gmail.com>
2024-03-01 19:26:55 +0100

    dev

--- scripts/aboutme.js
+++ scripts/aboutme.js
@@ -12,4 +12,37 @@ function showButtons() {
     })
 }
 
-showButtons()
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+      showButtons()
+      
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+const elements = document.querySelectorAll('.aboutme-social-icon');
+
+// Observe each element
+elements.forEach(element => {
+  observer.observe(element);
+});
+
+const scrollToTop = document.querySelector('#vissza-fel')
+
+scrollToTop.onclick = (e) => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+window.addEventListener('scroll', e => {
+  if (window.scrollY > 100) {
+    scrollToTop.classList.remove("hidden-button")
+  } else {
+    scrollToTop.classList.add("hidden-button")
+  }
+})
