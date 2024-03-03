fcbc231a8b795511621059ec9632a0447a418fc3
JaniBT <janoscsordas072@gmail.com>
2024-03-03 16:43:33 +0100

    dev

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -13,11 +13,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.transform = "translateX(100%)"
 }
-
-const munkak_dropdown = document.querySelector('#munkaim')
-
-munkak_dropdown.addEventListener('change', e => {
-    if (parseInt(munkak_dropdown.value) === 0) {
-        window.open('../subpages/munkaink.html', '_parent')
-    }
-})
